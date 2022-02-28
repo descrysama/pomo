f4ca0bb886c6d9b04805494acb35bb7a051054ab
DescrySama <pierrehugot35@gmail.com>
2022-02-28 20:07:28 +0100

    'test'

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ let breakTime = 4;
 let sec = 60;
 let stopwatch = false;
 let mybreak = false;
-let thecycle = 0;
+let startWorkingSound = new Audio('workingtime.mp3');
+let startbreakSound = new Audio('breaktime.mp3');
+startbreakSound.volume = 0.3
 
 function startWork() {
   if (stopwatch == false) {
     stopwatch = true;
     min = workTime;
     time = setInterval(cycle, 1000);
+    workingTimeBanner();
+    startbreakSound.pause();
+    startbreakSound.currentTime = 0;
+    startWorkingSound.play();
   }
 
   document.getElementById('startbutton').disabled = true
@@ -22,19 +28,25 @@ function startWork() {
 
 
 function stop() {
-  clearInterval(time)
-  if (mybreak == true) {
-    breakTime = min;
-  } else if (mybreak == false) {
-    workTime = min;
+  if (stopwatch == true) {
+    clearInterval(time)
+    if (mybreak == true) {
+      breakTime = min;
+    } else if (mybreak == false) {
+      workTime = min;
+    }
+    stopwatch = false;
+    startWorkingSound.pause();
+    startWorkingSound.currentTime = 0;
+    startbreakSound.play()
+    document.getElementById('startbutton').disabled = false
+    document.getElementById('pausebutton').disabled = true
+    document.querySelector('#myimg').src = "breaktime.png";
   }
-  stopwatch = false;
-  document.getElementById('startbutton').disabled = false
-  document.getElementById('pausebutton').disabled = true
-  document.querySelector('#myimg').src = "breaktime.png";
 }
 
 function reset(){
+  clearBanner();
   clearInterval(time)
   workTime = 24;
   breakTime = 4;
@@ -42,8 +54,34 @@ function reset(){
   stopwatch = false;
   document.getElementById('time').innerHTML = "00"+":"+"00";
   document.getElementById('title').innerHTML = "00"+":"+"00";
+  document.querySelector('#myimg').src = "";
   document.getElementById('startbutton').disabled = false
   document.getElementById('pausebutton').disabled = false
+}
+function workingTimeBanner() {
+  document.getElementById('status').innerHTML = '';
+  let creatediv = document.createElement('div');
+  let createh3 = document.createElement('h3');
+  createh3.setAttribute('class', 'text-danger');
+  createh3.textContent = 'Working Time';
+  creatediv.setAttribute('class', 'background-danger');
+  creatediv.append(createh3);
+  document.getElementById('status').appendChild(creatediv);
+}
+
+function clearBanner() {
+  document.getElementById('status').innerHTML = '';
+}
+
+function breakTimeBanner() {
+  document.getElementById('status').innerHTML = '';
+  let creatediv = document.createElement('div');
+  let createh3 = document.createElement('h3');
+  createh3.setAttribute('class', 'text-success');
+  createh3.textContent = 'Break Time';
+  creatediv.setAttribute('class', 'background-success');
+  creatediv.append(createh3);
+  document.getElementById('status').appendChild(creatediv);
 }
 
 function cycle() {
@@ -57,13 +95,13 @@ function cycle() {
     min = breakTime;
     sec = 60;
     mybreak = true;
-    thecycle++;
+    breakTimeBanner()
   } else if (min == 0 && mybreak == true){
     workTime = 24;
     min = workTime;
     sec = 60;
-    mybreak = false
-    thecycle++;
+    mybreak = false;
+    workingTimeBanner()
   }
 
   document.getElementById('time').innerHTML = min+":"+sec;
